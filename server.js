5c53f434e480d6616319dfc5066bd757db7b3a3d
Stacey Sugiono <sugiono.stacey@gmail.com>
2019-12-10 11:17:58 -0800

    a user can search for substrings

--- server.js
+++ server.js
@@ -15,37 +15,23 @@ app.use(function validateBearerToken(req, res, next) {
     const apiToken = process.env.API_TOKEN;
     const authToken = req.get('Authorization');
 
-    console.log('validate bearer token middleware')
     if (!authToken || authToken.split(' ')[1] !== apiToken) {
         return res.status(401).json({ error: 'Unauthorized request'})
     }
     next()
 })
 
-//search for Movies by genre, country, or avg_vote
 app.get('/movie', function handleGetMovies (req, res) {
     let response = MOVIEDEX;
-
     const { genre, country, avg_vote } = req.query;
     
-    
     if (genre) {
-        const distinctGenres = [...new Set(response.map( movie => movie.genre.toLowerCase()))];
-        const searchedGenre = genre.toLowerCase();
-        if (!distinctGenres.includes(searchedGenre)){
-            return res.status(400).send('Genre not found. Please try again.')
-        }
-        
         response = response.filter( movie =>
             movie.genre.toLowerCase().includes(genre.toLowerCase())
         )
     }
+
     if (country) {
-        const distinctCountries = [...new Set(response.map( movie => movie.country.toLowerCase()))];
-        const searchedCountry = country.toLowerCase();
-        if (!distinctCountries.includes(searchedCountry)){
-            return res.status(400).send('Country not found. Please try again')
-        }
         response = response.filter( movie => 
             movie.country.toLowerCase().includes(country.toLowerCase())
         )
